8a572a10357a17f8286db94b2d2202e55105d310
高山 周太郎 <takayamashutaro@LAB-N1652.local>
2019-07-29 16:59:04 +0900

    STEP6 ソーシャルログイン

--- src/routes/web.php
+++ src/routes/web.php
@@ -21,3 +21,10 @@ Route::get('/hoge', 'HogeController@index');
 
 Route::get('/bbs', 'BbsController@index');
 Route::post('/bbs', 'BbsController@create');
+
+Route::get('github', 'Github\GithubController@top');
+Route::post('github/issue', 'Github\GithubController@createIssue');
+Route::get('login/github', 'Auth\LoginController@redirectToProvider');
+Route::get('login/github/callback', 'Auth\LoginController@handleProviderCallback');
+
+Route::post('user', 'User\UserController@updateUser');
