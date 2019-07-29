1e658dd3c0e7da50720ff03280b9904167c75fef
高山 周太郎 <takayamashutaro@LAB-N1652.local>
2019-07-29 17:43:09 +0900

    step7 画像表示

---
+++ src/app/Http/Controllers/HomeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Img;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $img_paths = Img::all();
+        return view('home', ['img_paths'=>$img_paths]);
+    }
+
+    /**
+     * ファイルアップロード処理
+     */
+    public function upload(Request $request)
+    {
+        $this->validate($request, [
+            'file' => [
+                // 必須
+                'required',
+                // アップロードされたファイルであること
+                'file',
+                // 画像ファイルであること
+                'image',
+                // MIMEタイプを指定
+                'mimes:jpeg,png',
+            ]
+        ]);
+
+        if ($request->file('file')->isValid([])) {
+            $path = $request->file->store('public');
+            Img::insert(['img_path'=>basename($path)]);
+            return view('home')->with('filename', basename($path));
+        } else {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->withErrors();
+        }
+    }
+}
