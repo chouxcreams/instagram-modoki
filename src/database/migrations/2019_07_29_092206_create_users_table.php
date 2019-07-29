7c07662a9a3c5f5963016e3cccdcc049f1c53e08
高山 周太郎 <takayamashutaro@LAB-N1652.local>
2019-07-29 18:42:51 +0900

    STEP 9 instaもどき用のDB設計

---
+++ src/database/migrations/2019_07_29_092206_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->string('comment')->nullable();
+            $table->string('github_id');
+            $table->string('icon_file');
+            $table->bigInteger('num_of_likes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
