87347b2978d4fe24bdcdfb1bf96fbf5604edec78
高山 周太郎 <takayamashutaro@LAB-N1652.local>
2019-07-31 18:53:57 +0900

    いいね合計のインクリメント

--- src/app/Http/Controllers/LikeController.php
+++ src/app/Http/Controllers/LikeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Model\Like;
 use App\Model\User;
+use App\Model\Post;
 use Storage;
 
 class LikeController extends Controller
@@ -27,16 +28,33 @@ class LikeController extends Controller
     }
 
     public function like(Request $request) {
+        $post_id = $request->post('post_id');
         $like = [
-            'post_id' => $request->post('post_id'),
+            'post_id' => $post_id,
             'user_id' => $request->post('user_id')
         ];
         Like::insert($like);
+
+        Post::where('id', $post_id)->increment('num_of_likes');
+
+        $poster = Post::select('user_id')->where('id', $post_id)->get();
+        User::where('id', $poster[0]['user_id'])->increment('num_of_likes');
+
         return redirect('/');
     }
 
     public function dislike(Request $request) {
-        Like::destroy($request->post('like_id'));
+        $like_id = $request->post('like_id');
+
+        $like = Like::select('post_id', 'user_id')->where('id', $like_id)->get();
+        $post_id = $like[0]['post_id'];
+        $user_id = $like[0]['user_id'];
+
+        Post::where('id', $post_id)->decrement('num_of_likes');
+        
+        User::where('id', $user_id)->decrement('num_of_likes');
+
+        Like::destroy($like_id);
         return redirect('/');
     }
 }
